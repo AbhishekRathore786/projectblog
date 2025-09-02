09a59b6228beb57d25f9f91ba6902b1d73b1050e
AbhishekRathore <abhijyouti787@gmail.com>
2025-09-02 11:32:53 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://projectblog-frontend.onrender.com',
     credentials:true
 }))
 const _dirname = path.resolve()
